913066667b5bc8fb152836b15df4710b79cf712b
Pe3et <wolfpeeet@googlemail.com>
2024-10-14 17:39:06 +0200

    tried overlay in despair

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function openOverlayAddContact() {
-    document.getElementById('overlayContainer').classList.remove('displayNone');
+    // document.getElementById('overlayContainer').classList.remove('displayNone');
     document.getElementById('addContactCardOverlay').classList.remove('hidden')
 }
    
 function closeOverlayAddContact() {
-    document.getElementById('overlayContainer').classList.add('displayNone')
+    // document.getElementById('overlayContainer').classList.add('displayNone');
     document.getElementById('addContactCardOverlay').classList.add('hidden');
 }
 
