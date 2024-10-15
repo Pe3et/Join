a73022732a6e5db1b7fc67abb8e387d01678e161
Pe3et <wolfpeeet@googlemail.com>
2024-10-15 10:30:52 +0200

    overlay slide in in desktop version complete

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function openOverlayAddContact() {
-    // document.getElementById('overlayContainer').classList.remove('displayNone');
+    document.getElementById('overlayContainer').classList.add('overlayAppear');
+    document.getElementById('overlayContainer').classList.add('overlayBackgroundColor');
     document.getElementById('addContactCardOverlay').classList.add('slideInRight');
-    document.getElementById('overlayContainer').classList.add('slideInRight');
 
 }
    
 function closeOverlayAddContact() {
     // document.getElementById('overlayContainer').classList.add('displayNone');
-    document.getElementById('addContactCardOverlay').classList.add('hidden');
+    // document.getElementById('addContactCardOverlay').classList.add('hidden');
 }
 
 
