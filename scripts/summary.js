b79a0214838b362f29e1bdd46efe77ff8e41b02a
Pe3et <wolfpeeet@googlemail.com>
2024-10-30 18:46:45 +0100

    100% summary

--- scripts/summary.js
+++ scripts/summary.js
@@ -1,6 +1,8 @@
 
 function initSummary() {
     greeting();
+    document.querySelectorAll('.summaryCard').forEach( sc => sc.addEventListener('click', () => location.href = './board.html'));
+    getStats();
 }
 
 function greeting() {
@@ -8,6 +10,7 @@ function greeting() {
     daytimeGreeting();
     joinStorage.iconInitials == 'G' ? greetGuest() : greetUser();
     greetingContainer.classList.add('greetingAnimation');
+    setTimeout(()=>{greetingContainer.style.display = 'none'}, 1600);
 }
 
 function daytimeGreeting() {
@@ -36,4 +39,25 @@ function greetUser() {
     const greetingTextRef = document.getElementById('greetingText');
     greetingTextRef.innerText += ',';
     document.getElementById('userName').innerText = joinStorage.userName;
+}
+
+async function getStats() {
+    await getTasksFromDB();
+    const category = ['toDo', 'done', 'inProgress', 'awaitFeedback']
+    category.forEach( cat => renderStatusStat(cat));
+    renderUrgentStat();
+    document.getElementById('totalTasksSummary').innerText = tasks.length;
+
+}
+
+function renderStatusStat(stat='') {
+    const statCount = tasks.filter( task => task.status == stat).length;
+    document.getElementById(stat + 'Summary').innerText = statCount;
+}
+
+function renderUrgentStat() {
+    const urgentCount = tasks.filter( task => task.prio == 'urgent').length;
+    document.getElementById('urgentSummary').innerText = urgentCount;
+    tasks.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
+    document.getElementById('urgentDate').innerText = tasks[0].dueDate
 }
